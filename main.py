42dd17d6ed10f5ccb99be0857a335a74a704007e
plurmp <plurmpmcflurnt@gmail.com>
2022-01-12 20:09:46 -0600

    bug fix

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ for k in data.keys():
 	words.add(k)
 
 excluded_letters = ['a', 'b']  # excluded letters go here, enclosed by '' and separated by commas
-included_letters = ['e', 'g']  # included letters go here
+included_letters = ['x', 'h']  # included letters go here
 for s in words:
+	wrong_state = False
 	for e in excluded_letters:
 		if e in s:
-			continue
+			wrong_state = True
 	for i in included_letters:
 		if i not in s:
-			continue
+			wrong_state = True
+	if wrong_state:
+		continue
 	# included letters with a specific position go here, e.g. "a" in the third position is "..a.."
 	word_match = re.match(r'^.e...$', s)
 	if word_match:
