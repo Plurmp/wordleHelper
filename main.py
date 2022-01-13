e0311a7cbc5858069a0067ca1344fee27fdb8d26
plurmp <plurmpmcflurnt@gmail.com>
2022-01-12 19:52:36 -0600

    more useful for a human being

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ words = set()
 for k in data.keys():
 	words.add(k)
 
+excluded_letters = ['a', 'b']  # excluded letters go here, enclosed by '' and separated by commas
+included_letters = ['e', 'g']  # included letters go here
 for s in words:
-	if not ('a' in s and 'o' in s and 'r' in s):
-		continue
-	word_match = re.match(r'^[acefgjklmoqrsvwxyz]{5}$', s)
+	for e in excluded_letters:
+		if e in s:
+			continue
+	for i in included_letters:
+		if i not in s:
+			continue
+	# included letters with a specific position go here, e.g. "a" in the third position is "..a.."
+	word_match = re.match(r'^.e...$', s)
 	if word_match:
 		print(s)
